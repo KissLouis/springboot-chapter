f979db2c3f48dbefbf8fb310adfd3f928849721b
louis <javalouis@163.com>
2019-05-13 22:17:16 +0800

    Spring Boot快速入门案例：HelloWorld

---
+++ chapter01-01-helloworld/src/main/java/com/springboot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "helloWorld";
+    }
+
+}
