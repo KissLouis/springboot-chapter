516cc0c3f83213142597e6e8ce1eb2f5ebb51ece
louis <javalouis@163.com>
2019-05-25 17:14:29 +0800

    Spring Boot配置管理：Properties配置

--- chapter01-02-properties/src/main/java/com/springboot/controller/HelloController.java
+++ chapter01-02-properties/src/main/java/com/springboot/controller/HelloController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springboot.entity.Properties1;
-import springboot.entity.Properties2;
+import com.springboot.entity.Properties1;
+import com.springboot.entity.Properties2;
 
 @RestController
 public class HelloController {
