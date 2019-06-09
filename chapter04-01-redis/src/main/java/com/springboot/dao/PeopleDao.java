8d6c5df0e7e9a80882f028125abb31475fd1b0eb
louis <javalouis@163.com>
2019-06-09 21:31:38 +0800

    Spring Boot整合Redis及Redis工具类

---
+++ chapter04-01-redis/src/main/java/com/springboot/dao/PeopleDao.java
@@ -0,0 +1,27 @@
+package com.springboot.dao;
+
+import com.springboot.entity.People;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author Louis
+ * @title: PeopleDao
+ * @projectName springbootstudy
+ * @description: TODO
+ * @date 2019/5/28 10:36
+ */
+@Mapper
+public interface PeopleDao {
+
+    void save(People people);
+
+    void delete(@Param("id") Integer id);
+
+    void update(People people);
+
+    List<Object> find(@Param("id") Integer id);
+
+}
---
+++ chapter04-01-redis/src/main/java/com/springboot/entity/People.java
@@ -0,0 +1,25 @@
+package com.springboot.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author Louis
+ * @title: People
+ * @projectName springbootstudy
+ * @description: TODO
+ * @date 2019/5/27 15:02
+ */
+@Data
+public class People {
+
+    private Integer peopleId;
+    private String peopleName;
+    private String password;
+    private String salt;
+    private Integer status;
+    private Date modifyTime;
+    private Date createTime;
+
+}
