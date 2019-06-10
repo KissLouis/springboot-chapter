f6a38f219473828d3b53802085c6496df84e1616
KissLouis <javalouis@163.com>
2019-06-10 21:41:35 +0800

    Spring Boot整合Redis及Redis工具类

--- chapter04-01-redis/src/main/java/com/springboot/config/RedisConfig.java
+++ chapter04-01-redis/src/main/java/com/springboot/config/RedisConfig.java
@@ -19,26 +19,29 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @Configuration
 public class RedisConfig {
-    @Bean
-    @SuppressWarnings("all")
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-        template.setConnectionFactory(factory);
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        // key采用String的序列化方式
-        template.setKeySerializer(stringRedisSerializer);
-        // hash的key也采用String的序列化方式
-        template.setHashKeySerializer(stringRedisSerializer);
-        // value序列化方式采用jackson
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-        // hash的value序列化方式采用jackson
-        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-        template.afterPropertiesSet();
-        return template;
-    }
+	@Bean
+	@SuppressWarnings("all")
+	public RedisTemplate<String, Object> redisTemplate(
+			RedisConnectionFactory factory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+		template.setConnectionFactory(factory);
+		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(
+				Object.class);
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setVisibility(PropertyAccessor.ALL,
+				JsonAutoDetect.Visibility.ANY);
+		objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+		jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+		StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+		// key采用String的序列化方式
+		template.setKeySerializer(stringRedisSerializer);
+		// hash的key也采用String的序列化方式
+		template.setHashKeySerializer(stringRedisSerializer);
+		// value序列化方式采用jackson
+		template.setValueSerializer(jackson2JsonRedisSerializer);
+		// hash的value序列化方式采用jackson
+		template.setHashValueSerializer(jackson2JsonRedisSerializer);
+		template.afterPropertiesSet();
+		return template;
+	}
 }
--- chapter04-01-redis/src/main/java/com/springboot/controller/PeopleController.java
+++ chapter04-01-redis/src/main/java/com/springboot/controller/PeopleController.java
@@ -18,69 +18,69 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/people/")
 public class PeopleController {
 
-    @Autowired
-    private PeopleService peopleService;
+	@Autowired
+	private PeopleService peopleService;
 
-    /**
-     * @return com.springboot.util.result.Result
-     * @description: //TODO 查询全部用户
-     * @author Louis
-     * @date 2019/5/28 10:49
-     */
-    @GetMapping("/")
-    public Result findAll() {
-        return new Result(ResultCode.SUCCESS, peopleService.findAll());
-    }
+	/**
+	 * @return com.springboot.util.result.Result
+	 * @description: //TODO 查询全部用户
+	 * @author Louis
+	 * @date 2019/5/28 10:49
+	 */
+	@GetMapping("/")
+	public Result findAll() {
+		return new Result(ResultCode.SUCCESS, peopleService.findAll());
+	}
 
-    /**
-     * @param [id]
-     * @return com.springboot.util.result.Result
-     * @description: //TODO 根据Id查询用户
-     * @author Louis
-     * @date 2019/5/28 10:49
-     */
-    @GetMapping("/{id}")
-    public Result getOne(@PathVariable("id") Integer id) {
-        return new Result(ResultCode.SUCCESS, peopleService.findById(id));
-    }
+	/**
+	 * @param [id]
+	 * @return com.springboot.util.result.Result
+	 * @description: //TODO 根据Id查询用户
+	 * @author Louis
+	 * @date 2019/5/28 10:49
+	 */
+	@GetMapping("/{id}")
+	public Result getOne(@PathVariable("id") Integer id) {
+		return new Result(ResultCode.SUCCESS, peopleService.findById(id));
+	}
 
-    /**
-     * @param [id]
-     * @return com.springboot.util.result.Result
-     * @description: //TODO 根据Id删除用户
-     * @author Louis
-     * @date 2019/5/28 10:50
-     */
-    @DeleteMapping("/{id}")
-    public Result delete(@PathVariable("id") Integer id) {
-        peopleService.delete(id);
-        return new Result(ResultCode.SUCCESS);
-    }
+	/**
+	 * @param [id]
+	 * @return com.springboot.util.result.Result
+	 * @description: //TODO 根据Id删除用户
+	 * @author Louis
+	 * @date 2019/5/28 10:50
+	 */
+	@DeleteMapping("/{id}")
+	public Result delete(@PathVariable("id") Integer id) {
+		peopleService.delete(id);
+		return new Result(ResultCode.SUCCESS);
+	}
 
-    /**
-     * @param [people]
-     * @return com.springboot.util.result.Result
-     * @description: //TODO 新增用户
-     * @author Louis
-     * @date 2019/5/28 10:50
-     */
-    @PostMapping("/")
-    public Result save(People people) {
-        peopleService.save(people);
-        return new Result(ResultCode.SUCCESS);
-    }
+	/**
+	 * @param [people]
+	 * @return com.springboot.util.result.Result
+	 * @description: //TODO 新增用户
+	 * @author Louis
+	 * @date 2019/5/28 10:50
+	 */
+	@PostMapping("/")
+	public Result save(People people) {
+		peopleService.save(people);
+		return new Result(ResultCode.SUCCESS);
+	}
 
-    /**
-     * @param [people]
-     * @return com.springboot.util.result.Result
-     * @description: //TODO 修改用户
-     * @author Louis
-     * @date 2019/5/28 10:50
-     */
-    @PutMapping("/")
-    public Result update(People people) {
-        peopleService.update(people);
-        return new Result(ResultCode.SUCCESS);
-    }
+	/**
+	 * @param [people]
+	 * @return com.springboot.util.result.Result
+	 * @description: //TODO 修改用户
+	 * @author Louis
+	 * @date 2019/5/28 10:50
+	 */
+	@PutMapping("/")
+	public Result update(People people) {
+		peopleService.update(people);
+		return new Result(ResultCode.SUCCESS);
+	}
 
 }
