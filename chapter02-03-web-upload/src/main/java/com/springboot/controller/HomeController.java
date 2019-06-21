797b117f5fa1a419926c97ad0e4ae5a78b85b10d
KissLouis <javalouis@163.com>
2019-06-21 21:09:30 +0800

    Spring Boot整合文件上传：单文件上传，多文件上传，七牛云上传

---
+++ chapter02-03-web-upload/src/main/java/com/springboot/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("")
+	public String index() {
+		return "upload";
+	}
+
+
+}
