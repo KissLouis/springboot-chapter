48c6fff462575b47d0c3de44162cfaebf44c160e
KissLouis <javalouis@163.com>
2019-06-21 21:09:30 +0800

    Spring Boot整合文件上传：单文件上传，多文件上传，七牛云上传

---
+++ chapter02-03-web-upload/src/main/java/com/springboot/Application.java
@@ -0,0 +1,33 @@
+package com.springboot;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+		LOGGER.info("Spring Boot:Application Started Success!!!");
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory tomcatEmbedded() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+			if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>) {
+				((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+			}
+		});
+		return tomcat;
+	}
+
+}
