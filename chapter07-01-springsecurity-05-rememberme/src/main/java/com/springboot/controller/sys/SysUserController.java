6a5e7878af66b6b5dab371a19c85a23e15dc2dab
KissLouis <javalouis@163.com>
2019-07-02 20:38:46 +0800

    Spring Boot整合Spring Security实现安全管理-入门（五）：实现"记住我"功能

---
+++ chapter07-01-springsecurity-05-rememberme/src/main/java/com/springboot/controller/sys/SysUserController.java
@@ -0,0 +1,43 @@
+package com.springboot.controller.sys;
+
+
+import com.springboot.entity.sys.SysUser;
+import com.springboot.service.sys.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
+
+/**
+ * <p>
+ * 用户表 前端控制器
+ * </p>
+ *
+ * @author Louis
+ * @since 2019-06-29
+ */
+@Controller
+public class SysUserController {
+
+    @Autowired
+    private SysUserService sysUserService;
+
+    @GetMapping("/user")
+    public String user(@AuthenticationPrincipal Principal principal, Model model) {
+        model.addAttribute("username", principal.getName());
+        return "user/user";
+    }
+
+    @PostMapping("/register")
+    public String doRegister(SysUser user, Model model) {
+        // 此处省略校验逻辑
+        sysUserService.insert(user);
+        return "redirect:register?success=true";
+    }
+
+
+}
---
+++ chapter07-01-springsecurity-05-rememberme/src/main/java/com/springboot/entity/sys/SysUserRole.java
@@ -0,0 +1,29 @@
+package com.springboot.entity.sys;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 用户角色关系表
+ * </p>
+ *
+ * @author Louis
+ * @since 2019-06-29
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class SysUserRole implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String userId;
+
+    private String roleId;
+
+
+}
