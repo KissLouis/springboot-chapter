2bed6ecc4ae2ab7a3f4c5778413c6b61e8c918e6
KissLouis <1778282478@qq.com>
2023-07-26 13:30:26 +0800

    Spring Boot入门：HelloWorld

---
+++ spring-boot-01-basic/spring-boot-helloWorld/src/test/java/com/louis/springboothelloWorld/SpringBootHelloWorldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.louis.springboothelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Hello Spring Boot");
+	}
+
+}
