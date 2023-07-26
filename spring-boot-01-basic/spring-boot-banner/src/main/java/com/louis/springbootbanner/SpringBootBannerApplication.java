4ff05d12599b243726e4afb1bcb26d269d64f9a6
KissLouis <1778282478@qq.com>
2023-07-26 13:30:40 +0800

    Spring Boot基础配置：banner启动图配置

---
+++ spring-boot-01-basic/spring-boot-banner/src/main/java/com/louis/springbootbanner/SpringBootBannerApplication.java
@@ -0,0 +1,14 @@
+package com.louis.springbootbanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBannerApplication.class, args);
+	}
+
+
+}
